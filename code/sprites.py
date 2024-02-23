000d761d305feefe27b81cc74b59a2c045e29a28
Chen-Yu Liu <shin7965977@gmail.com>
2024-02-23 23:53:13 +0100

    background and paddle

---
+++ code/sprites.py
@@ -0,0 +1,41 @@
+import pygame
+from settings import *
+from random import choice, randint
+
+class Player(pygame.sprite.Sprite):
+	def __init__(self,groups):
+		super().__init__(groups)
+
+		# setup
+		self.image = pygame.Surface((WINDOW_WIDTH // 10, WINDOW_HEIGHT // 20))
+		self.image.fill('white')
+
+		# position
+		self.rect = self.image.get_rect(midbottom = (WINDOW_WIDTH // 2,WINDOW_HEIGHT - 20))
+		self.direction = pygame.math.Vector2()
+		self.pos = pygame.math.Vector2(self.rect.topleft)
+		self.speed = 300
+
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+	def screen_constraint(self):
+		if self.rect.right > WINDOW_WIDTH:
+			self.rect.right = WINDOW_WIDTH
+			self.pos.x = self.rect.x
+		if self.rect.left < 0:
+			self.rect.left = 0
+			self.pos.x = self.rect.x
+
+	def update(self,dt):
+		self.input()
+		self.pos.x += self.direction.x * self.speed * dt
+		self.rect.x = round(self.pos.x) 
+		self.screen_constraint()
