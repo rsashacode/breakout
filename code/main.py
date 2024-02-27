601b1dc4b2300e99d81cdd2a21809e0adc31af39
Chen-Yu Liu <shin7965977@gmail.com>
2024-02-27 21:44:42 +0100

    add funtion:basic blocks

--- code/main.py
+++ code/main.py
@@ -1,16 +1,6 @@
 import pygame, sys, time
 from settings import *
-from sprites import Player, Ball, Scoreboard
-
-
-def create_bg():
-    bg_original = pygame.image.load('assets/background/background.jpg').convert()
-    scale_factor = WINDOW_HEIGHT / bg_original.get_height()
-    scaled_width = bg_original.get_width() * scale_factor
-    scaled_height = bg_original.get_height() * scale_factor
-    scaled_bg = pygame.transform.scale(bg_original, (scaled_width, scaled_height))
-    return scaled_bg
-
+from sprites import Player, Ball, Scoreboard, Block
 
 class Game:
     def __init__(self):
@@ -21,17 +11,40 @@ class Game:
         pygame.display.set_caption('Breakout')
 
         # background
-        self.bg = create_bg()
+        self.bg = self.create_bg()
         
         
 
         # sprites group setup
         self.all_sprites = pygame.sprite.Group()
+        self.block_sprites = pygame.sprite.Group()
 
         # setup
         self.player = Player(self.all_sprites)
-        self.ball = Ball(self.all_sprites,self.player)
+        self.stage_setup()
+        self.ball = Ball(self.all_sprites,self.player,self.block_sprites)
         self.scoreboard = Scoreboard(self.all_sprites)
+        
+    
+    def create_bg(self):
+        bg_original = pygame.image.load('assets/background/background.jpg').convert()
+        scale_factor = WINDOW_HEIGHT / bg_original.get_height()
+        scaled_width = bg_original.get_width() * scale_factor
+        scaled_height = bg_original.get_height() * scale_factor
+        scaled_bg = pygame.transform.scale(bg_original, (scaled_width, scaled_height))
+        return scaled_bg
+    
+    def stage_setup(self):
+        # cycle through all rows and columns of BLOCK_MAP
+        for row_index, row in enumerate(BLOCK_MAP):
+            for col_index, col in enumerate(row):
+                if col != ' ':
+                    # find the x and y position for each individual block
+                    x = col_index * (BLOCK_WIDTH + GAP_SIZE) + GAP_SIZE // 2
+                    y = row_index * (BLOCK_HEIGHT + GAP_SIZE) + GAP_SIZE // 2
+                    Block(col,(x,y),[self.all_sprites,self.block_sprites])
+
+
     
     def run(self):
         last_time = time.time()
