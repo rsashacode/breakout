0fe03568b6f31374a8755abeee2c193b50199fb5
Aleksandr <al.d.rykov@outlook.com>
2024-02-29 19:25:34 +0100

    update paddle behaviour, tunneling dropped

--- main.py
+++ main.py
@@ -29,48 +29,95 @@ class Game:
         # sprites group setup
         self.all_sprites = pygame.sprite.Group()
         self.block_sprites = pygame.sprite.Group()
+        self.ball_sprites = pygame.sprite.Group()
 
-        # setup
-        self.player = Player(self.all_sprites)
-        self.stage_setup()
-        self.ball = Ball(groups=self.all_sprites, player=self.player, blocks=self.block_sprites)
-        self.scoreboard = Scoreboard(self.all_sprites)
-    
-    def stage_setup(self):
+        # initialise_game
+        self.player: Player = self.player_setup()
+        self.blocks: list[Block] = self.blocks_setup()
+        self.balls: list[Ball] = self.balls_setup(self.player)
+        self.scoreboard: Scoreboard = self.scoreboard_setup()
+
+    def player_setup(self) -> Player:
+        player_image = pygame.Surface(size=(settings.PADDLE_WIDTH, settings.PADDLE_HEIGHT))
+        player_image.fill('white')
+        player_rect = player_image.get_rect(midbottom=(settings.GAME_WINDOW_WIDTH // 2, settings.WINDOW_HEIGHT - 20))
+        return Player(
+            groups=self.all_sprites,
+            image=player_image,
+            rect=player_rect
+        )
+
+    def blocks_setup(self) -> list[Block]:
         # cycle through all rows and columns of BLOCK_MAP
+        blocks = []
         for row_index, row in enumerate(settings.BLOCK_MAP):
             for col_index, health in enumerate(row):
                 if health != ' ':
+                    health = int(health)
                     # find the x and y position for each individual block
                     x = col_index * (settings.BLOCK_WIDTH + settings.GAP_SIZE) + settings.GAP_SIZE // 2
                     y = row_index * (settings.BLOCK_HEIGHT + settings.GAP_SIZE) + settings.GAP_SIZE // 2
-                    Block(int(health), (x, y), [self.all_sprites, self.block_sprites])
+
+                    block_image = pygame.Surface(size=(settings.BLOCK_WIDTH, settings.BLOCK_HEIGHT))
+                    block_image.fill(color=settings.COLOR_LEGEND[health])
+
+                    block_rect = block_image.get_rect(topleft=(x, y))
+                    blocks.append(
+                        Block(
+                            groups=[self.all_sprites, self.block_sprites],
+                            image=block_image,
+                            rect=block_rect,
+                            health=health
+                        )
+                    )
+        return blocks
+
+    def balls_setup(self, player):
+        ball_image = pygame.image.load('./assets/other/Ball.png').convert_alpha()
+        balls = [
+            Ball(
+                groups=[self.all_sprites, self.ball_sprites],
+                image=ball_image,
+                rect=ball_image.get_rect(midbottom=player.rect.midtop),
+                player=player,
+                blocks=self.block_sprites
+            )
+        ]
+        return balls
+
+    def scoreboard_setup(self):
+        scoreboard_image = pygame.image.load('assets/other/scoreboard.jpg').convert_alpha()
+        scoreboard_image = pygame.transform.scale(
+            scoreboard_image,
+            (settings.SCOREBOARD_WIDTH, settings.WINDOW_HEIGHT)
+        )
+        scoreboard_rect = scoreboard_image.get_rect(topright=(settings.WINDOW_WIDTH, 0))
+        return Scoreboard(
+            groups=self.all_sprites,
+            image=scoreboard_image,
+            rect=scoreboard_rect
+        )
 
     def run(self):
         last_time = time.time()
 
         clock = pygame.time.Clock()
         while True:
-            clock.tick(settings.FPS)
-
-            delta_time = time.time() - last_time
-            last_time = time.time()
+            delta_time = clock.tick_busy_loop(settings.FPS) / 1000
+            fps = clock.get_fps()
 
             # Event handling
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_SPACE:
-                        self.ball.active = True
 
             keys_pressed = pygame.key.get_pressed()
 
             # update the game
             self.player.update(delta_time, keys_pressed)
             self.block_sprites.update()
-            self.ball.update(delta_time)
+            self.ball_sprites.update(delta_time, keys_pressed)
 
             # draw the frame
             self.display_surface.blit(source=self.bg, dest=(0, 0))
--- sprites.py
+++ sprites.py
@@ -1,24 +1,30 @@
 import pygame
 import settings
 import random
+import math
 
 from pygame.sprite import AbstractGroup
 
 
-class Player(pygame.sprite.Sprite):
-	def __init__(self, groups: AbstractGroup):
+class GameSprite(pygame.sprite.Sprite):
+	def __init__(self, groups, image: pygame.Surface, rect: pygame.Rect):
 		super().__init__(groups)
 
-		# setup
-		self.image = pygame.Surface(size=(settings.PADDLE_WIDTH, settings.PADDLE_HEIGHT))
-		self.image.fill('white')
-
-		# position
-		self.rect = self.image.get_rect(midbottom=(settings.GAME_WINDOW_WIDTH // 2, settings.WINDOW_HEIGHT - 20))
-		self.last_frame_rect = self.rect.copy()
-
-		self.direction = pygame.math.Vector2()
+		self.image = image
+		self.rect = rect
 		self.position = pygame.math.Vector2(self.rect.topleft)
+
+		self.last_frame_rect = self.rect.copy()
+		self.last_frame_position = self.position.copy()
+
+	def update(self, *args, **kwargs):
+		raise NotImplemented('Sprite must implement "update" method')
+
+
+class Player(GameSprite):
+	def __init__(self, groups: AbstractGroup, image: pygame.Surface, rect: pygame.Rect):
+		super().__init__(groups, image, rect)
+		self.direction = pygame.math.Vector2()
 
 	def check_screen_constraint(self):
 		if self.rect.right > settings.GAME_WINDOW_WIDTH:
@@ -28,6 +34,9 @@ class Player(pygame.sprite.Sprite):
 		if self.rect.left < 0:
 			self.rect.left = 0
 			self.position.x = self.rect.x
+
+	def move_paddle(self, delta_time):
+		self.position.x += self.direction.x * settings.DEFAULT_PADDLE_SPEED * delta_time
 
 	def update(self, delta_time, keys_pressed: pygame.key.ScancodeWrapper):
 		self.last_frame_rect = self.rect.copy()
@@ -40,32 +49,21 @@ class Player(pygame.sprite.Sprite):
 			self.direction.x = 0
 
 		self.check_screen_constraint()
-
-		self.position.x += self.direction.x * settings.DEFAULT_PADDLE_SPEED * delta_time
+		self.move_paddle(delta_time)
 		self.rect.x = round(self.position.x)
 
 
-class Scoreboard(pygame.sprite.Sprite):
-	def __init__(self, groups):
-		super().__init__(groups)
-		self.image = pygame.image.load('assets/other/scoreboard.jpg').convert_alpha()
-		self.image = pygame.transform.scale(self.image, (settings.SCOREBOARD_WIDTH, settings.WINDOW_HEIGHT))
-		self.rect = self.image.get_rect(topright=(settings.WINDOW_WIDTH, 0))
+class Scoreboard(GameSprite):
+	def __init__(self, groups, image: pygame.Surface, rect: pygame.Rect):
+		super().__init__(groups, image, rect)
 
 
-class Block(pygame.sprite.Sprite):
-	def __init__(self, health: int, pos: tuple[int, int], groups):
-		super().__init__(groups)
+class Block(GameSprite):
+	def __init__(self, groups, image: pygame.Surface, rect: pygame.Rect, health: int):
+		super().__init__(groups, image, rect)
 
 		# damage information
 		self.health = health
-
-		# Image
-		self.image = pygame.Surface(size=(settings.BLOCK_WIDTH, settings.BLOCK_HEIGHT))
-		self.image.fill(color=settings.COLOR_LEGEND[self.health])
-
-		self.rect = self.image.get_rect(topleft=pos)
-		self.last_frame_rect = self.rect.copy()
 
 	def get_damage(self, amount: int):
 		self.health -= amount
@@ -78,40 +76,18 @@ class Block(pygame.sprite.Sprite):
 			self.kill()
 
 
-class Ball(pygame.sprite.Sprite):
-	def __init__(self, groups, player: Player, blocks: [Block]):
-		super().__init__(groups)
+class Ball(GameSprite):
+	def __init__(self, groups, image: pygame.Surface, rect: pygame.Rect, player: Player, blocks: [Block]):
+		super().__init__(groups, image, rect)
 
 		# collision objects
 		self.player = player
 		self.blocks = blocks
 
-		# graphics setup
-		self.image = pygame.image.load('./assets/other/Ball.png').convert_alpha()
-
-		# position setup
-		self.rect = self.image.get_rect(midbottom=player.rect.midtop)
-		self.position = pygame.math.Vector2(x=self.rect.topleft)
 		self.direction = pygame.math.Vector2(x=(random.choice((1, -1)), -1))
 		self.speed = settings.DEFAULT_BALL_SPEED
-		self.last_frame_rect = self.rect.copy()
-		self.last_frame_position = self.position.copy()
 
 		self.active = False
-
-	def randomize_direction(self):
-		self.direction.x *= random.randrange(
-			100 - settings.DIRECTION_RANDOMIZATION,
-			100 + settings.DIRECTION_RANDOMIZATION) / 100
-		self.direction.y *= random.randrange(
-			100 - settings.DIRECTION_RANDOMIZATION,
-			100 + settings.DIRECTION_RANDOMIZATION) / 100
-
-	def get_overlapping_sprites(self) -> [pygame.sprite.Sprite]:
-		overlap_sprites = pygame.sprite.spritecollide(self, self.blocks, False)
-		if self.rect.colliderect(self.player.rect):
-			overlap_sprites.append(self.player)
-		return overlap_sprites
 
 	def movement(self, delta_time):
 		self.position.x += self.direction.x * self.speed * delta_time
@@ -142,53 +118,116 @@ class Ball(pygame.sprite.Sprite):
 		elif self.rect.bottom > settings.GAME_WINDOW_HEIGHT:
 			self.active = False
 
-	def sprites_collision(self):
+	def get_overlapping_sprites(self) -> [pygame.sprite.Sprite]:
+		overlap_sprites = pygame.sprite.spritecollide(self, self.blocks, False)
+		if self.rect.colliderect(self.player.rect):
+			overlap_sprites.append(self.player)
+		return overlap_sprites
+
+	def get_overlapping_rect(self, colliding_sprites) -> pygame.rect.Rect:
+		total_overlap_left = settings.GAME_WINDOW_WIDTH
+		total_overlap_right = 0
+		total_overlap_top = settings.GAME_WINDOW_HEIGHT
+		total_overlap_bottom = 0
+
+		# Calculate the overall area of overlapping
+		for sprite in colliding_sprites:
+			overlap = self.rect.clip(sprite.rect)
+			if overlap.left < total_overlap_left:
+				total_overlap_left = overlap.left
+			if overlap.right > total_overlap_right:
+				total_overlap_right = overlap.right
+			if overlap.top < total_overlap_top:
+				total_overlap_top = overlap.top
+			if overlap.bottom > total_overlap_bottom:
+				total_overlap_bottom = overlap.bottom
+
+		# Keyword arguments do not work here
+		overlap_rect = pygame.rect.Rect(
+			total_overlap_left,  # left
+			total_overlap_top,  # top
+			abs(total_overlap_right - total_overlap_left),  # width
+			abs(total_overlap_bottom - total_overlap_top)  # height
+		)
+		return overlap_rect
+
+	def handle_block_bounce(self, overlapping_rect: pygame.rect.Rect) -> None:
+		# Vertical collision
+		if overlapping_rect.width > overlapping_rect.height:
+			if self.direction.y < 0:
+				self.rect.top = overlapping_rect.bottom
+			else:
+				self.rect.bottom = overlapping_rect.top
+			self.direction.y *= -1
+		# Horizontal collision
+		elif overlapping_rect.height > overlapping_rect.width:
+			if self.direction.x < 0:
+				self.rect.left = overlapping_rect.right
+			else:
+				self.rect.right = overlapping_rect.left
+			self.direction.x *= -1
+		# Vertical and horizontal collision (perfectly hit an angle)
+		else:
+			if self.direction.x < 0:
+				self.rect.left = overlapping_rect.right
+			else:
+				self.rect.right = overlapping_rect.left
+			self.direction.x *= -1
+			if self.direction.y < 0:
+				self.rect.top = overlapping_rect.bottom
+			else:
+				self.rect.bottom = overlapping_rect.top
+			self.direction.y *= -1
+
+	def handle_paddle_collision(self, overlapping_rect: pygame.rect.Rect):
+		# Vertical collision
+		if overlapping_rect.width > overlapping_rect.height:
+			if self.direction.y < 0:
+				self.rect.top = overlapping_rect.bottom
+			else:
+				self.rect.bottom = overlapping_rect.top
+		# Horizontal collision
+		elif overlapping_rect.height > overlapping_rect.width:
+			self.active = False
+		# Vertical and horizontal collision (perfectly hit an angle)
+		else:
+			self.active = False
+
+		hit_point_x = overlapping_rect.centerx
+		paddle_middle = self.player.rect.centerx
+		paddle_width = self.player.rect.width
+
+		dist_from_paddle_center = hit_point_x - paddle_middle
+		angle_ratio = abs(dist_from_paddle_center) / (paddle_width / 2)
+		if angle_ratio != 0:
+			resulting_angle = math.pi / 2 - angle_ratio * (math.pi / 2 - math.pi / 6)
+			resulting_cotangent = 1 / math.tan(resulting_angle)
+
+			if dist_from_paddle_center > 0:
+				self.direction.x = resulting_cotangent * abs(self.direction.y)
+			else:
+				self.direction.x = -1 * resulting_cotangent * abs(self.direction.y)
+		else:
+			self.direction.x = 0
+		self.direction.y *= -1
+
+	def sprite_collisions(self):
 		# Get colliding sprites
 		colliding_sprites = self.get_overlapping_sprites()
 		if len(colliding_sprites) > 0:
-			# Introducing variables
-			total_overlap_left = settings.GAME_WINDOW_WIDTH
-			total_overlap_right = 0
-			total_overlap_top = settings.GAME_WINDOW_HEIGHT
-			total_overlap_bottom = 0
+			overlap_rect = self.get_overlapping_rect(colliding_sprites=colliding_sprites)
+			if self.player not in colliding_sprites:
 
-			# Calculate the overall area of overlapping
-			for sprite in colliding_sprites:
-				overlap = self.rect.clip(sprite.rect)
-				if overlap.left < total_overlap_left:
-					total_overlap_left = overlap.left
-				if overlap.right > total_overlap_right:
-					total_overlap_right = overlap.right
-				if overlap.top < total_overlap_top:
-					total_overlap_top = overlap.top
-				if overlap.bottom > total_overlap_bottom:
-					total_overlap_bottom = overlap.bottom
-				if getattr(sprite, 'health', None):
-					sprite.get_damage(1)
+				for sprite in colliding_sprites:
+					if getattr(sprite, 'health', None):
+						sprite.get_damage(1)
 
-			collision_rectangle_width = abs(total_overlap_left - total_overlap_right)
-			collision_rectangle_height = abs(total_overlap_top - total_overlap_bottom)
-
-			if collision_rectangle_width > collision_rectangle_height:
-				if self.direction.y < 0:
-					self.rect.top = total_overlap_bottom
-				else:
-					self.rect.bottom = total_overlap_top
-				self.direction.y *= -1
-			elif collision_rectangle_height == self.rect.height and collision_rectangle_width == self.rect.width:
-				raise RuntimeError("Tunneling!!!")
+				self.handle_block_bounce(overlapping_rect=overlap_rect)
 			else:
-				if self.direction.x < 0:
-					self.rect.left = total_overlap_right
-				else:
-					self.rect.right = total_overlap_left
-				self.direction.x *= -1
+				self.handle_paddle_collision(overlap_rect)
 
-			if self.player in colliding_sprites:
-				# ToDo Direction depending on where ball hits the paddle
-				self.randomize_direction()
+	def update(self, delta_time, keys_pressed: pygame.key.ScancodeWrapper):
 
-	def update(self, delta_time):
 		if self.active:
 
 			if self.direction.magnitude() != 0:
@@ -200,7 +239,11 @@ class Ball(pygame.sprite.Sprite):
 			self.movement(delta_time)
 
 			self.frame_collision()
-			self.sprites_collision()
+			self.sprite_collisions()
 		else:
 			self.rect.midbottom = self.player.rect.midtop
 			self.position = pygame.math.Vector2(self.rect.topleft)
+
+			if keys_pressed[pygame.K_SPACE]:
+				self.active = True
+				self.direction = pygame.math.Vector2(x=(random.choice((1, -1)), -1))
